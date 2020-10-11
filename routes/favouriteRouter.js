82ec9d3ff79235ec7eac85879fe42aa85618a7a0
Nehal Jaisalmeria <nehal.jaisalmeria14@gmail.com>
2020-10-11 12:11:39 +0530

    Integrating Client and Server

--- routes/favouriteRouter.js
+++ routes/favouriteRouter.js
@@ -67,7 +67,6 @@ favouriteRouter.route('/')
             })
             .catch((err) => next(err));
 })
-
 .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation is not supported on /favourites');
@@ -101,29 +100,27 @@ favouriteRouter.route('/')
 favouriteRouter.route('/:dishId')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
-    Favourites.find({})
-        .populate('user')
-        .populate('dishes')
-        .then((favourites) => {
-            if (favourites) {
-                const favs = favourites.filter(fav => fav.user._id.toString() === req.user.id.toString())[0];
-                const dish = favs.dishes.filter(dish => dish.id === req.params.dishId)[0];
-                if(dish) {
-                    res.statusCode = 200;
-                    res.setHeader("Content-Type", "application/json");
-                    res.json(dish);
-                } else {
-                    var err = new Error('You do not have dish ' + req.params.dishId);
-                    err.status = 404;
-                    return next(err);
-                }
-            } else {
-                var err = new Error('You do not have any favourites');
-                err.status = 404;
-                return next(err);
+    Favorites.findOne({user: req.user._id})
+    .then((favorites) => {
+        if (!favorites) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            return res.json({"exists": false, "favorites": favorites});
+        }
+        else {
+            if (favorites.dishes.indexOf(req.params.dishId) < 0) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": false, "favorites": favorites});
             }
-        }, (err) => next(err))
-        .catch((err) => next(err));
+            else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                return res.json({"exists": true, "favorites": favorites});
+            }
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 .post(cors.corsWithOptions, authenticate.verifyUser, 
     (req, res, next) => {
@@ -154,7 +151,6 @@ favouriteRouter.route('/:dishId')
             })
             .catch((err) => next(err));
 })
-
 .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation is not supported on /favourites/:dishId');
--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,8 @@ const cors = require('./cors');
 router.use(bodyParser.json());
 
 /* GET users listing. */
+router.options('*', cors.corsWithOptions, (req, res) => { res.sendStatus(200); } )
+
 router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   res.send('respond with a resource');
 });
@@ -43,15 +45,29 @@ router.post('/signup', cors.corsWithOptions,(req, res, next) => {
   });
 });
 
-router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
-  var token = authenticate.getToken({
-    _id: req.user._id,
-    firstname: req.user.firstname,
-    lastname: req.user.lastname
-  });
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You are successfully logged in!'});
+router.post('/login', cors.corsWithOptions, (req, res, next) => {
+  
+  passport.authenticate('local', (err, user, info) => {
+    if (err)
+      return next(err);
+
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: false, status: 'Login Unsuccessful!', err: info});
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        res.statusCode = 401;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: false, status: 'Login Unsuccessful!', err: 'Could not log in user!'});          
+      }
+      var token = authenticate.getToken({_id: req.user._id});
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: true, status: 'Login Successful!', token: token});
+    });
+  }) (req, res, next);
 });
 
 router.get('/logout', cors.corsWithOptions, (req, res) => {
@@ -74,6 +90,25 @@ router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res
     res.setHeader('Content-Type', 'application/json');
     res.json({success: true, token: token, status: 'You are successfully logged in!'});
   }
+});
+
+router.get('/checkJWTtoken', cors.corsWithOptions, (req, res) => {
+  passport.authenticate('jwt', {session: false}, (err, user, info) => {
+    if (err)
+      return next(err);
+    
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT invalid!', success: false, err: info});
+    }
+    else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT valid!', success: true, user: user});
+
+    }
+  }) (req, res);
 });
 
 module.exports = router;
