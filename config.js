8d31350a5690e179c73491652c87aa5cfe32526f
Nehal Jaisalmeria <nehal.jaisalmeria14@gmail.com>
2020-10-13 00:18:28 +0530

    mongo path

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'mongoUrl' : 'mongodb+srv://nehal:nehal@cluster0.tm54d.mongodb.net/conFusion?retryWrites=true&w=majority',
     'facebook' : {
         clientId: '280141976359042',
         clientSecret: 'aa5c976527e3562005227722576f98cc'
